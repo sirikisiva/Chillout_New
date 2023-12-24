ad19ad2eea73dd2c11b775b4a304656bf00c3c46
sirikisiva <96252335+sirikisiva@users.noreply.github.com>
2023-12-24 20:48:29 +0530

    Update userinfo.jsx

--- Relaxapp/src/userinfo.jsx
+++ Relaxapp/src/userinfo.jsx
@@ -2,95 +2,286 @@ import React, { useState } from "react";
 import "./userinfo.css";
 import { useNavigate } from "react-router-dom";
 
-const UserInfo = ({ onClose }) => {
-  const navigate = useNavigate();
+const UserInfo = () => {
   const [gender, setGender] = useState("");
-  const [hobbies, setHobbies] = useState([]);
-  const [birthdate, setBirthdate] = useState("");
 
   const handleGenderChange = (event) => {
     setGender(event.target.value);
   };
 
-  const handleHobbiesChange = (event) => {
-    const { value, checked } = event.target;
-    if (checked) {
-      setHobbies((prevHobbies) => [...prevHobbies, value]);
-    } else {
-      setHobbies((prevHobbies) => prevHobbies.filter((hobby) => hobby !== value));
-    }
-  };
-
-  const handleBirthdateChange = (event) => {
-    setBirthdate(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    // Do something with the collected user information
-    // For example, send it to a server or perform further actions
-
-    // After handling the information, close the popup
-    onClose();
-  };
-
   return (
     <div className="d-flex userinfo">
-      <form onSubmit={handleSubmit}>
-        <div className="field">
-          <label className="labelName">Gender </label>
-          <select
-            name="gender"
-            className="inputField"
-            value={gender}
-            onChange={handleGenderChange}
-          >
-            <option value="male">Male</option>
-            <option value="female">Female</option>
-            <option value="other">Other</option>
-          </select>
-        </div>
+      <form className="qnsForm">
+        <div>
+          <label className="labelText">Select your gender:</label>
+          <div className="d-flex">
+            <div>
+              <label>
+                <input
+                  type="radio"
+                  name="gender"
+                  value="male"
+                  checked={gender === "male"}
+                  onChange={handleGenderChange}
+                />
+                <span className="choices">Male</span>
+              </label>
+            </div>
 
-        <div className="field">
-          <label className="labelName">Hobbies </label>
-          <div>
-            <label>
-              <input
-                type="checkbox"
-                name="hobby"
-                value="reading"
-                checked={hobbies.includes("reading")}
-                onChange={handleHobbiesChange}
-              />
-              Reading
-            </label>
-            <label>
-              <input
-                type="checkbox"
-                name="hobby"
-                value="sports"
-                checked={hobbies.includes("sports")}
-                onChange={handleHobbiesChange}
-              />
-              Sports
-            </label>
-            {/* Add more hobbies as needed */}
+            <div className="mrgn">
+              <label>
+                <input
+                  type="radio"
+                  name="gender"
+                  value="female"
+                  checked={gender === "female"}
+                  onChange={handleGenderChange}
+                />
+                <span className="choices">Female</span>
+              </label>
+            </div>
+
+            <div className="mrgn">
+              <label>
+                <input
+                  type="radio"
+                  name="gender"
+                  value="other"
+                  checked={gender === "other"}
+                  onChange={handleGenderChange}
+                />
+                <span className="choices">Other</span>
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="qnsMargin">
+          <label className="labelText">Date of Birth </label>
+          <input type="date" name="dob" className="qnsinputField" />
+        </div>
+        <div className="qnsMargin">
+          <label className="labelText">How do you prefer to relax? </label>
+          <div className="d-flex">
+            <div>
+              <label>
+                <input type="radio" name="relaxationPreference" />
+                <span className="choices">Alone</span>
+              </label>
+            </div>
+            <div className="mrgn">
+              <label lassName="mrgn">
+                <input type="radio" name="relaxationPreference" />
+                <span className="choices"> With Friends</span>
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="qnsMargin">
+          <label className="labelText">How do you relax? </label>
+          <div className="d-flex">
+            <div>
+              {" "}
+              <label>
+                <input
+                  type="checkbox"
+                  name="relaxationMethod"
+                  value="meditation"
+                />
+                <span className="choices">Meditation</span>
+              </label>
+            </div>
+            <div className="mrgn">
+              {" "}
+              <label>
+                <input
+                  type="checkbox"
+                  name="relaxationMethod"
+                  value="reading"
+                />
+                <span className="choices">Reading</span>
+              </label>
+            </div>
+            <div className="mrgn">
+              {" "}
+              <label>
+                <input
+                  type="checkbox"
+                  name="relaxationMethod"
+                  value="listeningToMusic"
+                />
+                <span className="choices">Listening to Music</span>
+              </label>
+            </div>
+            <div className="mrgn">
+              {" "}
+              <label>
+                <input
+                  type="checkbox"
+                  name="relaxationMethod"
+                  value="exercise"
+                />
+                <span className="choices">Exercise</span>
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="qnsMargin">
+          <label className="labelText">
+            What meditation methods do you enjoy?
+          </label>
+          <div className="d-flex">
+            <div>
+              {" "}
+              <label>
+                <input type="checkbox" name="meditationMethods" />
+                <span className="choices">Mindfulness</span>
+              </label>
+            </div>
+            <div className="mrgn">
+              {" "}
+              <label>
+                <input type="checkbox" name="meditationMethods" />
+                <span className="choices"> Guided Meditation</span>
+              </label>
+            </div>
+            <div className="mrgn">
+              {" "}
+              <label>
+                <input type="checkbox" name="meditationMethods" />
+                <span className="choices"> Transcendental Meditation</span>
+              </label>
+            </div>
           </div>
         </div>
 
-        <div className="field">
-          <label className="labelName">DATE OF BIRTH </label>
-          <input
-            type="date"
-            name="dob"
-            className="inputField"
-            value={birthdate}
-            onChange={handleBirthdateChange}
-          />
+        <div className="qnsMargin">
+          <label className="labelText">How long do you prefer your meditation sessions to be?</label>
+          <div className="d-flex">
+            <div>
+              {" "}
+              <label>
+                <input type="radio" name="preferredDuration" />
+                <span className="choices"> Short (5-10 minutes)</span>
+              </label>
+            </div>
+            <div className="mrgn">
+              {" "}
+              <label>
+                <input type="radio" name="preferredDuration" />
+                <span className="choices"> Medium (15-20 minutes)</span>
+              </label>
+            </div>
+            <div className="mrgn">
+              {" "}
+              <label>
+                <input type="radio" name="preferredDuration" />
+                <span className="choices">Long (30+ minutes)</span>
+              </label>
+            </div>
+          </div>
         </div>
 
-        <button type="submit">Submit</button>
+        <div className="qnsMargin">
+          <label className="labelText">What is your experience level with meditation?</label>
+          <div className="d-flex">
+            <div>
+              {" "}
+              <label>
+                <input type="radio" name="experienceLevel" />
+                <span className="choices">Beginner</span>
+              </label>
+            </div>
+            <div className="mrgn">
+              {" "}
+              <label>
+                <input type="radio" name="experienceLevel" />
+                <span className="choices">Intermediate</span>
+              </label>
+            </div>
+            <div className="mrgn">
+              {" "}
+              <label>
+                <input type="radio" name="experienceLevel" />
+                <span className="choices">Advanced</span>
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className="qnsMargin">
+          <label className="labelText">What is your favorite book genre?</label>
+          <div className="d-flex">
+            <div>
+              {" "}
+              <label>
+                <input type="checkbox" name="favoriteGenre" />
+                <span className="choices"> Fiction</span>
+              </label>
+            </div>
+            <div className="mrgn">
+              {" "}
+              <label>
+                <input type="checkbox" name="favoriteGenre" />
+                <span className="choices"> Non-Fiction</span>
+              </label>
+            </div>
+            <div className="mrgn">
+              {" "}
+              <label>
+                <input type="checkbox" name="favoriteGenre" />
+                <span className="choices">Mystery</span>
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className="qnsMargin">
+          <label className="labelText">Do you find it important to do exercise for relaxation?</label>
+          <div className="d-flex">
+            <div>
+              <label>
+                <input type="radio" name="exerciseImportance" />
+                <span className="choices">Yes</span>
+              </label>
+            </div>
+            <div className="mrgn">
+              {" "}
+              <label>
+                <input type="radio" name="exerciseImportance" />
+                <span className="choices">No</span>
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="qnsMargin">
+          <label className="labelText"> What kind of music do you prefer to relax yourself? </label>
+          <div className="d-flex">
+            <div>
+              {" "}
+              <label>
+                <input type="checkbox" name="preferredMusic" />
+                <span className="choices">Classical</span>
+              </label>
+            </div>
+            <div className="mrgn">
+              {" "}
+              <label>
+                <input type="checkbox" name="preferredMusic" />
+                <span className="choices">Ambient</span>
+              </label>
+            </div>
+
+            <div className="mrgn">
+              {" "}
+              <label>
+                <input type="checkbox" name="preferredMusic" />
+                <span className="choices">Soothing</span>
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div>Submit</div>
       </form>
     </div>
   );
