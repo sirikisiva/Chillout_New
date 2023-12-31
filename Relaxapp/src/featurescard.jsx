3970601a6707d5b7abf5093d97cc122a3f81b08d
sirikisiva <96252335+sirikisiva@users.noreply.github.com>
2023-12-31 14:59:01 +0530

    Update featurescard.jsx

--- Relaxapp/src/featurescard.jsx
+++ Relaxapp/src/featurescard.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './features.css';
 
-const FeaturesCard = ({ cards }) => {
+const FeaturesCard = ({ cards, onCardClick }) => {
     return (
         <div className="features-container">
             {cards.map((card, index) => (
-                <div key={index} className="feature-card">
+                <div key={index} onClick={() => onCardClick(card.id)} className="feature-card">
                     <div className='imgDiv'><img className='feature-img' src={card.img}></img></div>
                     <div className="feature-name">{card.name}</div>
                     <div className="feature-desc">{card.desc}</div>
