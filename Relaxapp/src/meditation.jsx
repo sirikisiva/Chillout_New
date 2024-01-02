7293fd6a67929ddc16a52bd1055c6fbcecca49a9
sirikisiva <96252335+sirikisiva@users.noreply.github.com>
2024-01-02 12:23:15 +0530

    Update meditation.jsx

--- Relaxapp/src/meditation.jsx
+++ Relaxapp/src/meditation.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React from 'react';
+import SideMenu from './sidemenu';
+import './features.css';
 
-const Meditation = () =>{
 
-    return(
-        <div>Meditation</div>
-    )
+const Meditation = () => {
+  return(
+    <div className='d-flex'>
+      <div className='col-3 side-menu'>
+        <SideMenu></SideMenu>
+      </div>
+      <div className='col-9'></div>
+      
+    </div>
+  )
+
 }
 
 export default Meditation;
