838d7d7ad5a5c3c4fa70c5f531ddb57d3e73064e
sirikisiva <96252335+sirikisiva@users.noreply.github.com>
2024-01-02 12:21:36 +0530

    Update books.jsx

--- Relaxapp/src/books.jsx
+++ Relaxapp/src/books.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React from 'react';
+import SideMenu from './sidemenu';
+import './features.css';
 
-const Books = () =>{
 
-    return(
-        <div>Books</div>
-    )
+const Books = () => {
+  return(
+    <div className='d-flex'>
+      <div className='col-3 side-menu'>
+        <SideMenu></SideMenu>
+      </div>
+      <div className='col-9'></div>
+      
+    </div>
+  )
+
 }
 
 export default Books;
