b07ee3ee98569bc77f6e44cd9231967d3eb6121b
sirikisiva <96252335+sirikisiva@users.noreply.github.com>
2024-01-02 12:22:39 +0530

    Update friend.jsx

--- Relaxapp/src/friend.jsx
+++ Relaxapp/src/friend.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React from 'react';
+import SideMenu from './sidemenu';
+import './features.css';
 
-const Friend = () =>{
 
-    return(
-        <div>Friend</div>
-    )
+const Friend = () => {
+  return(
+    <div className='d-flex'>
+      <div className='col-3 side-menu'>
+        <SideMenu></SideMenu>
+      </div>
+      <div className='col-9'></div>
+      
+    </div>
+  )
+
 }
 
 export default Friend;
