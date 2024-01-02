f37dfb61b717d4e41f401623fa5293334bba816a
sirikisiva <96252335+sirikisiva@users.noreply.github.com>
2024-01-02 12:24:04 +0530

    Update music.jsx

--- Relaxapp/src/music.jsx
+++ Relaxapp/src/music.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import SideMenu from './sidemenu';
+import './features.css';
+
 
 const Music = () => {
   return(
-    <div>Music</div>
+    <div className='d-flex'>
+      <div className='col-3 side-menu'>
+        <SideMenu></SideMenu>
+      </div>
+      <div className='col-9'>
+
+      </div>
+      
+    </div>
   )
 
 }
