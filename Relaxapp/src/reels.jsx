5e24e19e936321166d1de23b0db0aa47ad367a64
sirikisiva <96252335+sirikisiva@users.noreply.github.com>
2024-01-02 12:24:41 +0530

    Update reels.jsx

--- Relaxapp/src/reels.jsx
+++ Relaxapp/src/reels.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React from 'react';
+import SideMenu from './sidemenu';
+import './features.css';
 
-const Reels = () =>{
 
-    return(
-        <div>Reels</div>
-    )
+const Reels = () => {
+  return(
+    <div className='d-flex'>
+      <div className='col-3 side-menu'>
+        <SideMenu></SideMenu>
+      </div>
+      <div className='col-9'></div>
+      
+    </div>
+  )
+
 }
 
 export default Reels;
