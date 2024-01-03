5d3dfa122870a6205e0ca4b05d928636fc930f50
sirikisiva <96252335+sirikisiva@users.noreply.github.com>
2024-01-03 13:42:32 +0530

    Update meditation.jsx

--- Relaxapp/src/meditation.jsx
+++ Relaxapp/src/meditation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import SideMenu from './sidemenu';
 import './features.css';
+import DisplayMeditation from './displaymeditation';
 
 
 const Meditation = () => {
@@ -9,7 +10,9 @@ const Meditation = () => {
       <div className='col-3 side-menu'>
         <SideMenu></SideMenu>
       </div>
-      <div className='col-9'></div>
+      <div className='col-9'>
+      <DisplayMeditation></DisplayMeditation>
+      </div>
       
     </div>
   )
