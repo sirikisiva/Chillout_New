5a81d79c78f796e282bc923b25058757ee76ce7b
sirikisiva <96252335+sirikisiva@users.noreply.github.com>
2024-01-03 23:10:32 +0530

    Update sidemenu.jsx

--- Relaxapp/src/sidemenu.jsx
+++ Relaxapp/src/sidemenu.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import "./sidemenu.css";
+import "./sidemunu.css";
 
 const SideMenu = () => {
   const location = useLocation();
