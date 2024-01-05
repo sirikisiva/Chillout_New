f7dae7beb1c37097297ab50086897c9eba6db7ea
sirikisiva <96252335+sirikisiva@users.noreply.github.com>
2024-01-05 20:31:58 +0530

    Update loginscreen.jsx

--- Relaxapp/src/loginscreen.jsx
+++ Relaxapp/src/loginscreen.jsx
@@ -1,8 +1,5 @@
-
-
 import React, { useState } from "react";
 import "./loginscreen.css";
-// import { TextInput } from "./components/TextInput";
 import { useNavigate } from "react-router-dom";
 
 const LoginScreen = () => {
@@ -26,7 +23,8 @@ const LoginScreen = () => {
     navigate("/ForgotPasswordScreen");
   };
 
-  const goToFeaturesScreen = () => {
+  const goToFeaturesScreen = (e) => {
+    e.preventDefault();
     if (validateForm()) {
       navigate("/Features");
     }
@@ -62,86 +60,90 @@ const LoginScreen = () => {
 
   const handleInputChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+    validateForm();
   };
 
   return (
-    <div className="loginScreen">
-      {/* <div>
-          <h6>Nice to see you again</h6>
-          <h1>WELCOME BACK!</h1>
-        </div> */}
-      <div className="animCard">
-        <span className="logoName">ChillOUT</span>
-        <span className="logoCaption">chill vibe with chilling content</span>
-        {/* <img src="./logocircle.png" alt="logo" className="App-logo" /> */}
+    <div className="loginScreen d-flex">
+      <div className="loginContainer col-6">
+        <div className="card">
+          <div className="box1">
+            <div>
+              <h3 className="login">Login Account</h3>
+              <p className="caption">Welcome to our App</p>
+            </div>
+          </div>
+
+          <form>
+            <div className="box2">
+              <div className="inputContainer">
+                <div>
+                <label className="inputLabel">Username or Email</label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Username or Email"
+                  className="inputStyle"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                  onFocus={handleFocus}
+                  onBlur={handleBlur}
+                />
+                </div>
+                {errors.name && (
+                  <div className="error-message">{errors.name}</div>
+                )}
+              </div>
+
+              <div className="inputContainer">
+                <div>
+                <label className="inputLabel">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  className="inputStyle"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  onFocus={handleFocus}
+                  onBlur={handleBlur}
+                />
+                </div>
+                {errors.password && (
+                  <div className="error-message">{errors.password}</div>
+                )}
+              </div>
+            </div>
+            <div className="box3">
+              <div className="lineContainer">
+                <div className="checkboxContainer">
+                  <input className="checkbox" type="checkbox" />
+                  <label>Remember me</label>
+                </div>
+                <div
+                  className="forgotPassword"
+                  onClick={goToForgotPasswordScreen}
+                >
+                  Forgot password?
+                </div>
+              </div>
+
+              <button className="signInBtn" onClick={(e) => goToFeaturesScreen(e)}>
+                Login
+              </button>
+              <button className="signInBtn">Login with Company Email</button>
+            </div>
+          </form>
+
+          <div>
+            <h6 className="regHere" onClick={goToSignupScreen}>
+              Don't have an account? <span className="regBtn"> Register</span>
+            </h6>
+          </div>
+        </div>
       </div>
-
-      <div className="card">
-        
-        <div className="loginCardTitle">
-          <h1>Login Account</h1>
-          <p>Welcome to our App</p>
-        </div>
-        <div>
-          <div className="inputContainer">
-            <label className="inputLabel">Username or Email</label>
-            <input
-              type="text"
-              name="name"
-              placeholder="Username or Email"
-              className="inputStyle"
-              value={formData.name}
-              onChange={handleInputChange}
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-            />
-            {errors.name && <div className="error-message">{errors.name}</div>}
-          </div>
-
-          <div className="inputContainer">
-            <label className="inputLabel">Password</label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              className="inputStyle"
-              value={formData.password}
-              onChange={handleInputChange}
-              onFocus={handleFocus}
-              onBlur={handleBlur}
-            />
-            {errors.password && (
-              <div className="error-message">{errors.password}</div>
-            )}
-          </div>
-        </div>
-        <div>
-          <div className="lineContainer">
-            <div className="checkboxContainer">
-              <input className="checkbox" type="checkbox" />
-              <label>Remember me</label>
-            </div>
-            <div className="forgotPassword" onClick={goToForgotPasswordScreen}>
-              Forgot password?
-            </div>
-          </div>
-          <div className="loginBtnsContainer">
-            <button className="signInBtn" onClick={goToFeaturesScreen}>
-              Login
-            </button>
-            <button className="signInBtn">Login with Company Email</button>
-          </div>
-        </div>
-        <div>
-          <h6>
-            Don't have an account ?{" "}
-            <b>
-              <span className="regBtn" onClick={goToSignupScreen}>
-                Register
-              </span>
-            </b>
-          </h6>
-        </div>
+      <div className="titleAnim col-6">
+        <img src="./chillogo.png" alt="logo" />
       </div>
     </div>
   );
