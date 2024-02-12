d21482b3c504e91139a459236e3adeb2c330bdc6
sirikisiva <96252335+sirikisiva@users.noreply.github.com>
2024-02-12 20:37:57 +0530

    Add files via upload

--- Relaxapp/src/displaymusic.jsx
+++ Relaxapp/src/displaymusic.jsx
@@ -1,101 +1,67 @@
-// import React, { useState } from "react";
-// import AppLogo from "./appLogo";
-// import MusicCards from "./musicCards";
-// import MusicPopup from "./musicPopup";
-// import { calmMusicList, sleepMusicList } from "./musicData";
-
-// const DisplayMusic = () => {
-//   const [selectedCard, setSelectedCard] = useState(null);
-//   const [modalOpen, setModalOpen] = useState(false);
-
-//   const handleCardClick = (card) => {
-//     setSelectedCard(card);
-//     setModalOpen(true);
-//   };
-
-//   const handleCloseModal = () => {
-//     setSelectedCard(null);
-//     setModalOpen(false);
-//   };
-
-//   const musicCards = [
-//     {
-//       id: "one",
-//       img: "musicOne.jpg",
-//       name: "Calm Music",
-//       mapId: 1,
-//       calmSongs: [
-//         { song: "calmMusic.mp3", id: "calmone" },
-//         { song: "calmMusic.mp3", id: "calmtwo" },
-//         { song: "calmMusic.mp3", id: "calmthree" },
-//       ],
-//     },
-//     {
-//       id: "two",
-//       img: "musicTwo.jpg",
-//       name: "Stress Relief Music",
-//     },
-//     {
-//       id: "three",
-//       img: "musicThree.jpg",
-//       name: "Sleep Music",
-//     },
-//     {
-//       id: "four",
-//       img: "musicFour.jpg",
-//       name: "Depression Relief Music",
-//     },
-//     {
-//       id: "five",
-//       img: "musicFive.jpg",
-//       name: "Anxiety Relief Music",
-//     },
-//     {
-//       id: "six",
-//       img: "musicSix.jpeg",
-//       name: "Anger Relief Music",
-//     },
-//   ];
-
-//   return (
-//     <div>
-//       <div>
-//         <MusicCards musicCards={musicCards} onCardClick={handleCardClick} />
-//       </div>
-//       {modalOpen && selectedCard && (
-//         <MusicPopup
-//           modelTitle={selectedCard.name}
-//           calmSongs={selectedCard.calmSongs || []}
-//           onClose={handleCloseModal}
-//         />
-//       )}
-//     </div>
-//   );
-// };
-
-// export default DisplayMusic;
-
 import React, { useRef, useState, useEffect } from "react";
-import AppLogo from "./appLogo";
 import MusicCards from "./musicCards";
-import MusicPopup from "./musicPopup";
 import "./musicCards.css";
 import MusicList from "./musicList";
 import MusicTimer from "./musicTimer";
-import { calmMusicList, sleepMusicList } from "./musicData";
 
 const DisplayMusic = () => {
   const [selectedCard, setSelectedCard] = useState(null);
-  const [modalOpen, setModalOpen] = useState(false);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    let intervalId;
+
+    const startAutoScroll = () => {
+      const maxScroll = containerRef.current.scrollWidth - containerRef.current.clientWidth;
+      const targetScroll = containerRef.current.scrollLeft + 5;
+
+      containerRef.current.style.transition = "scroll 1s linear";
+
+      if (targetScroll >= maxScroll) {
+        setTimeout(() => {
+          containerRef.current.scrollLeft = 0;
+          containerRef.current.style.transition = "none";
+          setTimeout(() => {
+            containerRef.current.style.transition = "scroll 1s linear";
+            containerRef.current.scrollLeft = 5;
+          }, 0);
+        }, 1000);
+      } else if (targetScroll <= maxScroll) {
+        containerRef.current.scrollLeft = targetScroll;
+      }
+    };
+
+    const handleMouseEnter = () => {
+      clearInterval(intervalId);
+    };
+
+    const handleMouseLeave = () => {
+      intervalId = setInterval(() => startAutoScroll(), 200);
+    };
+
+    const container = containerRef.current;
+
+    if (container) {
+      container.addEventListener("mouseenter", handleMouseEnter);
+      container.addEventListener("mouseleave", handleMouseLeave);
+    }
+
+    intervalId = setInterval(() => startAutoScroll(), 200);
+
+    return () => {
+      const container = containerRef.current;
+
+      if (container) {
+        container.removeEventListener("mouseenter", handleMouseEnter);
+        container.removeEventListener("mouseleave", handleMouseLeave);
+      }
+
+      clearInterval(intervalId);
+    };
+  }, []);
 
   const handleCardClick = (card) => {
     setSelectedCard(card);
-    setModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setSelectedCard(null);
-    setModalOpen(false);
   };
 
   const musicCards = [
@@ -144,40 +110,26 @@ const DisplayMusic = () => {
     },
   ];
 
-  const containerRef = useRef(null);
-
-  const scrollBy = (scrollAmount) => {
-    if (containerRef.current) {
-      containerRef.current.scrollLeft += scrollAmount;
-    }
-  };
-
   return (
     <div>
       <div className="d-flex">
         <div className="col-10 cards-heading">
-          Dive into Relaxation with Our Collection of Soothing Music Cards
-        </div>
-        <div className="bees col-2" title="Click to scroll for more features">
-          <div onClick={() => scrollBy(-200)}>
-            <img className="leftBee" src="./move.png" alt="Left Bee" />
-          </div>
-          <div onClick={() => scrollBy(200)}>
-            <img className="rightBee" src="./move.png" alt="Right Bee" />
-          </div>
+          Dive into Relaxation with Our Collection of Soothing Music
         </div>
       </div>
       <div ref={containerRef} className="featuresBox">
         <MusicCards musicCards={musicCards} onCardClick={handleCardClick} />
       </div>
       <div className="d-flex">
-      <div className="col-8 selected-card-list">
-      {selectedCard && <MusicList list={selectedCard.calmSongs || []} heading={selectedCard.name} />}
+        <div className="col-8 selected-card-list">
+          {selectedCard && (
+            <MusicList list={selectedCard.calmSongs || []} heading={selectedCard.name} />
+          )}
+        </div>
+        <div className="col-4">
+          <MusicTimer></MusicTimer>
+        </div>
       </div>
-      <div className="col-4">
-       <MusicTimer></MusicTimer>
-      </div>
-   </div>
     </div>
   );
 };
